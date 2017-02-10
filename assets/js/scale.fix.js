8d7f8062861ef1667d9d7248fa9c180b829ba1f1
Aaron Meyer <a@ameyer.me>
2017-02-09 19:48:34 -0500

    Tweak

--- assets/js/scale.fix.js
+++ assets/js/scale.fix.js
@@ -1,27 +1,17 @@
-(function(document) {
-    var metas = document.getElementsByTagName('meta'),
-        changeViewportContent = function(content) {
-            for (var i = 0; i < metas.length; i++) {
-                if (metas[i].name == "viewport") {
-                    metas[i].content = content;
-                }
-            }
-        },
-        initialize = function() {
-            changeViewportContent("width=device-width, minimum-scale=1.0, maximum-scale=1.0");
-        },
-        gestureStart = function() {
-            changeViewportContent("width=device-width, minimum-scale=0.25, maximum-scale=1.6");
-        },
-        gestureEnd = function() {
-            initialize();
-        };
-
-
-    if (navigator.userAgent.match(/iPhone/i)) {
-        initialize();
-
-        document.addEventListener("touchstart", gestureStart, false);
-        document.addEventListener("touchend", gestureEnd, false);
+var metas = document.getElementsByTagName('meta');
+var i;
+if (navigator.userAgent.match(/iPhone/i)) {
+  for (i=0; i<metas.length; i++) {
+    if (metas[i].name == "viewport") {
+      metas[i].content = "width=device-width, minimum-scale=1.0, maximum-scale=1.0";
     }
-})(document);
+  }
+  document.addEventListener("gesturestart", gestureStart, false);
+}
+function gestureStart() {
+  for (i=0; i<metas.length; i++) {
+    if (metas[i].name == "viewport") {
+      metas[i].content = "width=device-width, minimum-scale=0.25, maximum-scale=1.6";
+    }
+  }
+}
